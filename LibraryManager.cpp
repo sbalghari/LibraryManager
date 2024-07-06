18db7b0030a7436835ba308c0ca3652cf61bad36
Saifullah Balghari <balgharisaifullah@gmail.com>
2024-07-06 13:24:19 +0500

    initial commit

--- LibraryManager.cpp
+++ LibraryManager.cpp
@@ -1,47 +1,73 @@
 #include<iostream>
-#include<string>
+#include<vector>
 
 using namespace std;
 
+string BOOK_NAME;
+std::vector<string> books;
+int NUMBER_OF_BOOKS;
+
 void add_books(){
 
-    string book_name;
+    while(true){    
+        cout << "Please enter the name of the book..." << endl;
+        cin >> BOOK_NAME;
+        
+        books.push_back(BOOK_NAME);
 
-    cout << "Please enter the name of the book..." << endl;
-    cin >> book_name;
-    cout << book_name << " added successfully..." << endl;
+        cout << BOOK_NAME << " added successfully..." << endl;
+        NUMBER_OF_BOOKS++;
 
+        char again;
+        cout << "Type \(y\) to add again and anything else to exit!" << endl;
+        cin >> again;
+
+        if(again == 'y'){
+            continue;
+        }
+        else{
+            break;
+        }
+        
+    }
 }
-
 
 void delete_books(){
+    cout << "Please enter the name of the book you want to delete..." << endl;
+    cin >> BOOK_NAME;
 
-}
-
-void update_books(){
-
-}
-
-void search_books(){
+    for(int i = 0; i < books.size(); i++){
+        if(BOOK_NAME == books[i]){
+            books.erase(books.begin() + i);
+            NUMBER_OF_BOOKS--;
+            cout << BOOK_NAME << " deleted successfully..." << endl;
+            break;
+        }
+        else{
+            cout << "Book not found..." << endl;
+            break;
+        }
+    }
 
 }
 
 void display_books(){
-
+    for(int i = 0; i < books.size(); i++){
+        cout << books[i] << endl;
+    }
 }
 
 void main_menu(){
 
     int choice;
 
-cout << "WellCome To The Library" 
-     << "What do you want to..." 
-     << "1.Add Books"
-     << "2.Delete Books"
-     << "3.Update Books"
-     << "4.Search Books"
-     << "5.Display Books"
-     << "6.Exit"
+
+cout << "WellCome To The Library\n" << "\n"
+     << "What do you want to...\n" 
+     << "1.Add Books\n"
+     << "2.Delete Books\n"
+     << "3.Display Books\n"
+     << "4.Exit\n"
      << endl;
 
 cin >> choice;
@@ -54,15 +80,9 @@ switch(choice){
         delete_books();
         break;
     case 3:
-        update_books();
-        break;
-    case 4:
-        search_books();
-        break;
-    case 5:
         display_books();
         break;
-    case 6:
+    case 4:
         exit(0);
         break;
     default:
@@ -73,7 +93,7 @@ switch(choice){
 
 int main (){
 
-    
+    main_menu();
 
     return 0;
 }
