f9e0a44799894e20e774ff5cd6aafaa126ed2294
Saifullah Balghari <balgharisaifullah@gmail.com>
2024-07-17 01:05:41 +0500

    lm v0.02

--- Library_Manager/LibraryManager.cpp
+++ Library_Manager/LibraryManager.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <fstream>
 #include <thread>
 #include <chrono>
@@ -10,231 +11,197 @@
 
 using namespace std;
 
-class LibraryManager{
-    /*  
-    * @brief Simple implementation of the library manager.
+class LibraryManager {
+    /*
+    Simple implementation of a library management system.
 
-    * This program provides the following functionalities to the user.
-    * -> Add books to the library.
-    * -> Remove books from the library
-    * -> Edit the books in the library
-    * -> Display the books in the library
-    * -> Save the books in the library
-    * 
-    * TODO: Try to make this libraryManager a GUI library manager.
-    * 
+    This program provides the following functionalities to the user.
+    -> Add books to the library.
+    -> Remove books from the library.
+    -> Edit the books in the library.
+    -> Display the books in the library.
+    -> Save the books in a text file.
+
     */
-    private:
-        string book_name;
-        vector<string> books;
-        int NUMBER_OF_BOOKS = 0;
-        const string books_txt = "books.txt";
+private:
+    string book_name;
+    vector<string> books;
+    const string books_txt = "books.txt";
 
-        void load_books(){
-            ifstream file(books_txt);
-            if(!file.is_open()){
-                cerr << "Error opening the file!";
+    void load_books() {
+        ifstream file(books_txt);
+        if (!file.is_open()) {
+            cerr << "Error opening the file!" << endl;
+            return;
+        }
+        string line;
+        while (getline(file, line)) {
+            books.push_back(line);
+        }
+        file.close();
+    }
+
+public:
+    LibraryManager() {
+        load_books();
+    }
+
+    void main() {
+        while (true) {
+            int choice;
+            cout << "----------------------------------------------------------------\n"
+                 << "---------------- WellCome To The Library Manager----------------\n"
+                 << "----------------------What you want to do?----------------------\n"
+                 << "-------------------------1.Add Books----------------------------\n"
+                 << "------------------------2.Delete Books--------------------------\n"
+                 << "------------------------3.Display Books-------------------------\n"
+                 << "------------------------4.Update Books--------------------------\n"
+                 << "-------------------------5.Save books---------------------------\n"
+                 << "----------------------------6.Exit------------------------------\n"
+                 << "----------------------------------------------------------------\n"
+                 << endl << ">";
+            cin >> choice;
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input! Please enter a number." << endl;
+                continue;
+            }
+            cin.ignore(); // Clear the buffer after reading choice
+            switch (choice) {
+                case 1:
+                    add_books();
+                    break;
+                case 2:
+                    delete_books();
+                    break;
+                case 3:
+                    display_books();
+                    break;
+                case 4:
+                    update_books();
+                    break;
+                case 5:
+                    save_books();
+                    break;
+                case 6:
+                    cout << "\nExiting...\n";
+                    return;
+                default:
+                    cout << "Invalid Choice! Please enter a number between 1 and 6." << endl;
+            }
+        }
+    }
+
+#pragma region Helper functions
+
+    void add_books() {
+        try {
+            while (true) {
+                cout << "Please enter the name of the book!" << endl;
+                getline(cin, book_name);
+                books.push_back(book_name);
+                cout << "\"" << book_name << "\"" << " added successfully." << endl;
+
+                char query;
+                cout << "Type (y) to add another book, or anything else to go back!" << endl;
+                cin >> query;
+                cin.ignore(); // Clear the buffer after reading query
+                if (query != 'y') {
+                    break;
+                }
+            }
+        } catch (const exception &e) {
+            cerr << e.what() << '\n';
+        }
+    }
+
+    void save_books() {
+        if (books.empty()) {
+            cout << "No books to save!" << endl;
+            return;
+        }
+        try {
+            ofstream file(books_txt);
+            if (!file.is_open()) {
+                cerr << "Unable to open file!" << endl;
                 return;
             }
-            string line;
-            while(getline(file, line)){
-                books.push_back(line);
-                NUMBER_OF_BOOKS++;
+            for (const auto &book : books) {
+                file << book << endl;
             }
+            cout << "Books saved successfully!" << endl;
             file.close();
+        } catch (const exception &e) {
+            cerr << e.what() << '\n';
         }
+    }
 
-    public:
-        LibraryManager(){
-            load_books();
-        };
-    
-        void main(){
-            while (true){
-                int choice;
-                cout<< "----------------------------------------------------------------\n"
-                    << "---------------- WellCome To The Library Manager----------------\n" 
-                    << "----------------------What you want to do?----------------------\n"
-                    << "-------------------------1.Add Books----------------------------\n"
-                    << "------------------------2.Delete Books--------------------------\n"
-                    << "------------------------3.Display Books-------------------------\n"
-                    << "------------------------4.Update Books--------------------------\n"
-                    << "-------------------------5.Save books---------------------------\n"
-                    << "----------------------------6.Exit------------------------------\n"
-                    << "----------------------------------------------------------------\n"
-                    << endl <<">";
-                cin >> choice;
-                if(cin.fail()) {
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                    cout << "Invalid input. Please enter a number." << endl;
-                    continue;
-                }
-                switch(choice){
-                    case 1:
-                        add_books();
-                        break;
-                    case 2:
-                        delete_books();
-                        break;
-                    case 3:
-                        display_books();
-                        break;
-                    case 4:
-                        update_books();
-                        break;
-                    case 5:
-                        save_books();
-                        break;
-                    case 6:
-                        cout << "\nExiting...\n";
-                        return;
-                    default:
-                        cout << "Invalid Choice!\nExiting..." << endl;
-                        return;
-                    }
-            }    
-        }
-
-        #pragma region Helper functions
-
-        void add_books(){
-
-            try{
-                while(true){
-                    cout << "Please enter the name of the book..." << endl;
-                    cin.ignore();
-                    getline(cin, book_name);
-                    
-                    books.push_back(book_name);
-
-                    cout << "\"" << book_name << "\"" << " added successfully..." << endl;
-                    NUMBER_OF_BOOKS++;
-
-                    char again;
-                    cout << "Type (y) to add again and anything else to go back!" << endl;
-                    cin >> again;
-
-                    if(again == 'y'){continue;}
-                    else{break;}
-                }
-            }
-            catch(const std::exception& e){
-                cerr << e.what() << '\n';
-            }
-        }
-
-        void save_books() {
-            if(books.empty()){
-                cout << "No books entered to save!" << endl;
+    void delete_books() {
+        try {
+            if (books.empty()) {
+                cout << "No books in the library!" << endl;
                 return;
             }
-            try{
-                ofstream file(books_txt);
-                if (!file.is_open()){
-                    cerr << "Unable to open file";
-                    return;
-                }
-                else{
-                    for(auto &book : books){
-                        file << book << endl;
-                    }
-                    cout << "Books saved successfully!" << endl;
-                    file.close();
-                }
-            }
-            catch(const std::exception& e){
-                cerr << e.what() << '\n';
-            }
-            cout << "Returning to main menu..." << endl;
-        }
-        void delete_books(){
-            try{
-                if(books.empty()){
-                cout << "No books in the library..." << endl;
-                return;
-                }
-                cout << "Please enter the name of the book you want to delete..." << endl;
-                cin.ignore();
-                getline(cin, book_name);
+            cout << "Please enter the name of the book you want to delete." << endl;
+            getline(cin, book_name);
 
-                bool found = false;
-                for(int i = 0; i < books.size(); i++){
-                    if(book_name == books[i]){
-                        books.erase(books.begin() + i);
-                        NUMBER_OF_BOOKS--;
-                        cout << "\"" <<book_name << "\"" << " deleted successfully..." << endl;
-                        found = true;
-                        break;
-                    }
-                }
-                if(!found){
-                    cout << "Book not found..." << endl;
-                    return;
-                }
+            auto it = find(books.begin(), books.end(), book_name);
+            if (it != books.end()) {
+                books.erase(it);
+                cout << "\"" << book_name << "\"" << " deleted successfully." << endl;
+            } else {
+                cout << "Book not found!" << endl;
             }
-            catch(const std::exception& e){
-                cerr << e.what() << '\n';
-            }
-            cout << "Returning to main menu..." << endl;
+        } catch (const exception &e) {
+            cerr << e.what() << '\n';
         }
+    }
 
-        void display_books(){
-            if(books.empty()){
-                cout << "No books in the library..." << endl;
+    void display_books() {
+        if (books.empty()) {
+            cout << "No books in the library!" << endl;
+            return;
+        }
+        cout << "\nBooks in the library:-" << endl;
+        cout << "------------------------" << endl;
+        for (const auto &book : books) {
+            cout << book << endl;
+        }
+        cout << "------------------------" << endl;
+        cout << "\nReturning to main menu..." << endl;
+        this_thread::sleep_for(chrono::seconds(3));
+    }
+
+    void update_books() {
+        try {
+            if (books.empty()) {
+                cout << "No books in the library!" << endl;
                 return;
             }
-            cout << "\nBooks in the library:" << endl;
-            cout << "------------------------" << endl;
-            for(const auto& book : books){
-                cout << book << endl;
+            cout << "Please enter the name of the book you want to update." << endl;
+            getline(cin, book_name);
+            cout << "Enter the new name of the book." << endl;
+            string new_name;
+            getline(cin, new_name);
+
+            auto it = find(books.begin(), books.end(), book_name);
+            if (it != books.end()) {
+                *it = new_name;
+                cout << "\"" << book_name << "\"" << " updated successfully to " << "\"" << new_name << "\"" << endl;
+            } else {
+                cout << "Book not found!" << endl;
             }
-            cout << "------------------------" << endl;
-            cout << "\nReturning to main menu..." << endl;
-            std::this_thread::sleep_for(std::chrono::seconds(3));
-                    
+        } catch (const exception &e) {
+            cerr << e.what() << '\n';
         }
+    }
 
-        void update_books(){
-            try{
-                if(books.empty()){
-                cout << "No books in the library..." << endl;
-                return;
-                }
-                cout << "Please enter the name of the book you want to update..." << endl;
-                cin.ignore();
-                getline(cin, book_name);
-                cout << "Enter the new name of the book..." << endl;
-                string new_name;
-                getline(cin, new_name);
-
-                bool found = false;
-
-                for(int i = 0; i < books.size(); i++){
-                    if(book_name == books[i]){
-                        found = true;
-                        books[i] = new_name;
-                        cout << "\"" << book_name << "\"" << " updated successfully to " << "\"" << new_name << "\"" << endl;
-                        break;
-                    } 
-                }
-                if(!found){
-                    cout << "Book not found..." << endl;
-                    return;
-                }
-            }
-            catch(const std::exception& e){
-                cerr << e.what() << '\n';
-            }
-            cout << "Returning to main menu..." << endl;
-        }
-        #pragma endregion
+#pragma endregion
 };
 
-int main(){
-
+int main() {
     LibraryManager MyLibrary;
     MyLibrary.main();
-
     return 0;
 }
